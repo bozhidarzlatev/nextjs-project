30424244d803e512acfb611a941aaa0239cd0107
bozhidarzlatev <156793066+bozhidarzlatev@users.noreply.github.com>
2026-07-27 17:33:45 +0300

    navigation qwerks

---
+++ my-app/app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+const Layout = ({children} : {children : React.ReactNode}) => {
+    return (
+        <div>
+            <p>
+            Navigation Bar
+            </p>
+            {children}
+            
+        </div>
+    )
+
+}
+
+export default Layout
--- my-app/app/(root)/page.tsx
+++ my-app/app/(root)/page.tsx
@@ -1,4 +1,4 @@
-import Hello from "./components/hello";
+import Hello from "../components/hello";
 
 const Home =  () => {
   console.log(`Wha type of next js am i?`);
