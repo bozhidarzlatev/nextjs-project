5c633e7c2912d79a09986511d54995acd7784535
bozhidarzlatev <156793066+bozhidarzlatev@users.noreply.github.com>
2026-07-27 18:51:35 +0300

    cashing

--- my-app/app/(dashboard)/books/page.tsx
+++ my-app/app/(dashboard)/books/page.tsx
@@ -1,4 +1,8 @@
+import { cacheLife } from 'next/cache'
+
 async function Page() {
+  'use cache'
+  cacheLife('hours')
   const responce = await fetch("http://localhost:3000/api/books");
   const books = await responce.json();
 
